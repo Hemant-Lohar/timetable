e70340475e3a1f23ec388a9386d30e79ec85aeeb
Hemant-Lohar <hemantlohar585@gmail.com>
2021-08-01 12:32:27 +0530

    Updated UI

--- script.js
+++ script.js
@@ -1,21 +1,95 @@
 const pe = document.querySelector(".pe");
 const pe1 = document.querySelector(".pe1");
+const pe2 = document.querySelector(".pe2");
+const pe3 = document.querySelector(".pe3");
+const pe4 = document.querySelector(".pe4");
+const oe = document.querySelector(".oe");
+const oe1 = document.querySelector(".oe1");
+const oe2 = document.querySelector(".oe2");
+
 const lect = document.querySelector(".lect");
 const lect1 = document.querySelector(".lect1");
+const lect2 = document.querySelector(".lect2");
+const lect3 = document.querySelector(".lect3");
+const lect4 = document.querySelector(".lect4");
+const oelect = document.querySelector(".oelect");
+const oelect1 = document.querySelector(".oelect1");
+const oelect2 = document.querySelector(".oelect2");
 
 lect.onclick = () => {
-    pe.style.display = "block"
+    pe.style.display = "block";
 
     setTimeout(() => {
-        pe.style.display = "none"
-    },3000);
+        pe.style.display = "none";
+    },5000);
 }
+
+// lect.forEach((lec) => lec.addEventListener('click', show)); 
+
+// function show() {
+//     pe.style.display = "block";
+
+//     setTimeout(() => {
+//         pe.style.display = "none";
+//     },5000);
+// }
 
 lect1.onclick = () => {
-    pe1.style.display = "block"
+    pe1.style.display = "block";
 
     setTimeout(() => {
-        pe1.style.display = "none"
-    },3000);  
+        pe1.style.display = "none";
+    },5000);
 }
+
+lect2.onclick = () => {
+    pe2.style.display = "block";
+
+    setTimeout(() => {
+        pe2.style.display = "none";
+    },5000);
+}
+
+
+lect3.onclick = () => {
+    pe3.style.display = "block";
+
+    setTimeout(() => {
+        pe3.style.display = "none";
+    },5000);
+}
+
+lect4.onclick = () => {
+    pe4.style.display = "block";
+
+    setTimeout(() => {
+        pe4.style.display = "none";
+    },5000);
+}
+
+oelect.onclick = () => {
+    oe.style.display = "block";
+
+    setTimeout(() => {
+        oe.style.display = "none";
+    },5000);
+}
+
+oelect1.onclick = () => {
+    oe1.style.display = "block";
+
+    setTimeout(() => {
+        oe1.style.display = "none";
+    },5000);
+}
+
+oelect2.onclick = () => {
+    oe2.style.display = "block";
+
+    setTimeout(() => {
+        oe2.style.display = "none";
+    },5000);
+}
+
+
 
